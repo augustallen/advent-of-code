367b1df6ab93baeb0c60038c1f3765468c272ac5
augustallen4@gmail.com <august.allen@recursionpharma.com>
2019-12-01 21:40:02 -0700

    added answers to both parts of the puzzle

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,7 +1,20 @@
 import math
 
-f = open("day1_module_mass.txt","r")
+# File provided at: https://adventofcode.com/2019/day/1
+file_path="Day 1\day1_module_mass.txt"
 
+f = open(file_path,"r")
+total_fuel_required = 0
+
+for mass in f:
+    total_fuel_required += math.floor(int(mass)/3) - 2
+
+print("Answer to Part 1: " + str(total_fuel_required))
+
+f.close()
+
+# Re-open file to re-iterate through the ffor loop, this time accounting for mass of fuel.
+f = open(file_path,"r")
 
 total_fuel_required = 0
 fuel_per_module = 0
@@ -14,4 +27,6 @@ for mass in f:
       additional_fuel = math.floor(int(additional_fuel)/3) - 2
     total_fuel_required += fuel_per_module
 
-print(total_fuel_required)
+print("Answer to Part 2: " + str(total_fuel_required))
+
+f.close()
