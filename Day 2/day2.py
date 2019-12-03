826518fa4478bf74fc085bee4e3e3204228e204b
augustallen <august.allen@recursionpharma.com>
2019-12-03 11:14:14 -0700

    answered part 1 w/function using for or while

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,20 +1,83 @@
-with open(r"Day 2\input.txt","r") as f:
+import traceback
+
+with open(r"Day 2/input.txt","r") as f:
     values = f.read().split(",")
 
-#Convert values in the list to integers.
+# Convert values in the list to integers.
 values = map(int, values)
 values = list(map(int, values))
 
-# Start at first opcode 
-iterator = 0
-opcode=values[iterator]
+"""
+# Working function for answer 1 but not answer 2, going to adjust from while loop to for loop to try and avoid index out of range issues
+def opcode_func(input_list,noun,verb):
+    values[1] = noun
+    values[2] = verb
+    pointer = 0
+    opcode=input_list[pointer]
+    while opcode!=99:
+        if opcode == 1:
+            input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] + input_list[input_list[pointer+2]]
+        if opcode == 2:
+            input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] * input_list[input_list[pointer+2]]
+        pointer += 4
+        opcode=input_list[pointer]
+    return input_list, pointer
+"""
 
-while opcode!=99:
-    if opcode == 1:
-        values[values[iterator+3]] = values[values[iterator+1]] + values[values[iterator+2]]
-    if opcode == 2:
-        values[values[iterator+3]] = values[values[iterator+1]] * values[values[iterator+2]]
-    iterator += 4
-    opcode=values[iterator]
+def opcode_func(input_list,noun,verb):
+    values[1] = noun
+    values[2] = verb
+    
+    for pointer in range(0,len(input_list),4):
+        opcode = input_list[pointer]
+        if opcode == 1:
+            input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] + input_list[input_list[pointer+2]]
+        if opcode == 2:
+            input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] * input_list[input_list[pointer+2]]
+        if opcode == 99:
+            break
+        opcode=input_list[pointer]
+    return input_list, pointer
+
+
+
+print("Answer to part 1: " + str(opcode_func(values,12,2)[0][0]))
+
+"""
+for noun in range(0,99):
+    try:
+        output,pointer = (opcode_func(values,noun,2))
+    except:
+        print("Input broke on noun " + str(noun) + ", pointer " + str(pointer) + ", and output: " + str(output))
+        print("")
+        traceback.print_exc()
+        break
+        #if opcode_func(values,noun,verb)[0] == 19690720:
+    #    break
+    #else:
+    #    continue
+    #break
+"""
+
+
+
+
+"""
+edited_values = values
 
 print(values)
+print(edited_values)
+while edited_values[0] != 19690720:
+    while opcode!=99:
+                if opcode == 1:
+                    edited_values[edited_values[pointer+3]] = edited_values[edited_values[pointer+1]] + edited_values[edited_values[pointer+2]]
+                if opcode == 2:
+                    edited_values[edited_values[pointer+3]] = edited_values[edited_values[pointer+1]] * edited_values[edited_values[pointer+2]]
+                pointer += 4
+                opcode=edited_values[pointer]
+    edited_values = values                          # Re-set edited values
+    for noun in range(0,99):                        # Cycle through all possible options for value 1, but need to figure out how to break if edited_values[0] = 19690720
+        values[1] = noun
+        for verb in range(0,99):
+            values[2] = verb
+"""
