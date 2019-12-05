93820541c7d0ce4017e2820f0ea91cd3bc6e09a3
augustallen4@gmail.com <august.allen@recursionpharma.com>
2019-12-04 20:23:12 -0700

    solved day 4

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # It is a six-digit number. Implicit in my range which all have six digits but useful for other ranges.
 
 def length_six(number):
@@ -27,6 +29,13 @@ def never_decrease(number):
                 return False
     return True
 
+def same_adjacent2(number): # If there are 2 of anything, they would have to be next to each other in order to also pass the never_decrease function
+    d = defaultdict(int)
+    for digit in str(number):
+        d[digit] += 1
+
+    return (2 in d.values())
+
 # Solving part 1.
 
 possible_count1 = 0
@@ -35,23 +44,7 @@ for password in range(138241,674035):
     if length_six(password) and same_adjacent(password) and never_decrease(password):
         possible_count1 += 1
 
-print("The answer to part 1: " + str(possible_count1))
-
 # Solving part 2.
-
-def same_adjacent2(number):
-    counter=0
-    for char in str(number):
-        if counter<(len(str(number))-1):
-            counter += 1
-            char_after = str(number)[counter]
-            char_before = str(number)[counter-2]
-            if char == char_after:
-                if counter>1 and not char == char_before:
-                    continue
-                if not char == char_before:
-                    return True
-    return False
 
 possible_count2 = 0
 
@@ -59,5 +52,7 @@ for password in range(138241,674035):
     if length_six(password) and same_adjacent2(password) and never_decrease(password):
         possible_count2 += 1
 
+
+print("The answer to part 1: " + str(possible_count1))
 
 print("The answer to part 2: " + str(possible_count2))
--- Day 4/scratch.py
+++ Day 4/scratch.py
@@ -1,28 +1,43 @@
 from collections import defaultdict
 
-words = "apple banana apple strawberry banana lemon"
 
-number = 123455
+def same_adjacent2(number): # If there are 2 of anything, they would have to be next to each other in order to also pass the never_decrease function
+    d = defaultdict(int)
+    for digit in str(number):
+        d[digit] += 1
 
-d = defaultdict(int)
-for digit in str(number):
-    d[digit] += 1
+    return (2 in d.values())
 
-print(d)
+print(same_adjacent2(111122))
 
-'''  
-def same_adjacent2(number):
+
+def same_adjacent_fails(number):
+    counter=0
+    for char in str(number):
+        if counter<(len(str(number))-1): # Skip if counter is on last character
+            counter += 1
+            char_after = str(number)[counter]
+            if char == char_after:
+                if counter == (len(str(number))-1):
+                    return True
+                if char == str(number)[counter+1] and counter == len(str(number)):
+                    return False
+                return True
+    return False
+
+
+def same_adjacent_fails2(number):
     counter=0
     for char in str(number):
         if counter<(len(str(number))-1):
             counter += 1
-            char_after = str(number)[counter]
-            char_before = str(number)[counter-2]
-            if counter>1 and char == char_after and not char == char_before:
-                return True
+        
+            if char == str(number)[counter]:
+                if counter == (len(str(number))-1):
+                    return True
+                if char == str(number)[counter+1]:
+                    return False
     return False
-'''
-# print(same_adjacent2(111222))
 
 
-# maybe write it a different way to just count the quantity of each number and search for numbers where there's at least one 2?
+
