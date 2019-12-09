0bd330d0451d9a19e39a2051f359f93597f88e97
augustallen4@gmail.com <august.allen@recursionpharma.com>
2019-12-08 18:19:05 -0700

    working on day 5

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,34 +1,32 @@
 import traceback
 
-with open(r"Day 5/input.txt","r") as f:
+with open(r"Day 5/input_test.txt","r") as f:
     values = f.read().split(",")
 
 # Convert values in the list to integers.
 values = map(int, values)
 values = list(map(int, values))
 
-def opcode_func(input_list,noun,verb):
-    input_list[1] = noun
-    input_list[2] = verb
-    for pointer in range(0,len(input_list),4):
+def opcode_func(input_list):
+    pointer=0
+    while pointer<len(values):
         opcode = input_list[pointer]
+        print(input_list)
         if opcode == 1:
-            input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] + input_list[input_list[pointer+2]]
+            input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] + input_list[input_list[pointer+2]]           
         if opcode == 2:
             input_list[input_list[pointer+3]] = input_list[input_list[pointer+1]] * input_list[input_list[pointer+2]]
+        if opcode == 3:
+            print(input_list[input_list[pointer+1]])
+            print(input_list[pointer+1])
+            input_list[input_list[pointer+1]] = input_list[pointer+1]
         if opcode == 99:
             break
-        opcode=input_list[pointer]
+        pointer += 1 # needs to be incremented by the number of instructions
     return input_list
 
 # Assign the list of values to be an editable list (without the colon slicing the list both variables would reference the same list)
 editable_values=values[:] 
 
-print("Answer to part 1: " + str(opcode_func(editable_values,12,2)[0]))
 
-for noun in range(100):
-    for verb in range(100):
-        editable_values = values[:]                             
-        output = (opcode_func(editable_values,noun,verb))
-        if output[0] == 19690720:
-            print("Answer to part 2: " + str(100*noun+verb))
+print(opcode_func(editable_values))
