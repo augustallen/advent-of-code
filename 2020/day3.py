417d1b56138aa437aae8f1176f2835b5ae7704fa
augustallen <august.allen@recursionpharma.com>
2020-12-05 21:18:15 -0700

    done

--- 2020/day3.py
+++ 2020/day3.py
@@ -13,4 +13,22 @@ def part1(lines):
             tree_count+=1
     return tree_count
 
-print(part1(lines))
+def tree_counter(lines, right, down):
+    col = 0
+    tree_count = 0
+    for row in range(down,len(lines),down):
+        col += right
+        if col > 30:
+            col -= 31
+        if lines[row][col] == "#":
+            tree_count+=1
+    return tree_count
+
+def part2(lines):
+    return (tree_counter(lines,1,1) * 
+            tree_counter(lines,3,1) *
+            tree_counter(lines,5,1) *
+            tree_counter(lines,7,1) *
+            tree_counter(lines,1,2))
+
+print(f"Part 1: {part1(lines)}\nPart 2: {part2(lines)}")
