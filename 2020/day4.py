1f826ad8026def891f74641d5e71971af7a14107
augustallen <august.allen@recursionpharma.com>
2020-12-05 22:44:03 -0700

    complete part 1

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,24 +1,24 @@
 with open('2020/input_day4.txt') as f:
     lines = [l.strip() for l in f]
 
-passports=[]
-passport = ""
-for line in lines:
-    if not line: 
-        passport = dict(p.split(":") for p in passport.lstrip().split(" "))
-        passports.append(passport) 
-        passport = ""
-    passport += " "
-    passport += line
-passport = dict(p.split(":") for p in passport.lstrip().split(" "))
-passports.append(passport)
 
+def part1(lines):
+    passports=[]
+    passport = ""
+    for line in lines:
+        if not line: # on an empty line reset the current passport after saving all it's k:v pairs
+            passport = dict(p.split(":") for p in passport.lstrip().split(" "))
+            passports.append(passport) 
+            passport = ""
+        passport += " "
+        passport += line
+    passport = dict(p.split(":") for p in passport.lstrip().split(" "))
+    passports.append(passport)
 
-valid_passports = 0
-for p in passports:
-    print(p)
-    if p.keys() >= {"byr", "iyr", "eyr" ,"hgt", "hcl", "ecl", "pid"}:
-        valid_passports += 1
-    # else:
-        #  print(f"Invalid passport:\n {p} \n {p.keys()}")
-print(valid_passports)
+    valid_passports = 0
+    for p in passports:
+        if p.keys() >= {"byr", "iyr", "eyr" ,"hgt", "hcl", "ecl", "pid"}:
+            valid_passports += 1
+    return valid_passports
+
+print(part1(lines))
