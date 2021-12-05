cb1a891ca03cbc7def48c16595a495beb2de7efa
augustallen <august.allen@envedabio.com>
2021-12-05 13:12:27 -0700

    done

---
+++ 2021/day1.py
@@ -0,0 +1,21 @@
+with open("2021/day1.txt") as f:
+    lines = [int(l.strip()) for l in f]
+
+
+def part1():
+    increases = 0
+    for i, l in enumerate(lines):
+        if l > lines[i-1] and i > 0:
+            increases += 1
+    return increases
+
+
+def part2():
+    increases = 0
+    for i, l in enumerate(lines):
+        if lines[i] > lines[i-3] and i > 2:
+            increases += 1
+    return increases
+
+
+print(f"Part 1: {part1()} \n Part 2: {part2()}")
